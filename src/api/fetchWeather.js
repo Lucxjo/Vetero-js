178771eda999f09332094e172d8537fa77362ca4
Ludoviko <louis@hollingworth.ch>
2021-05-14 20:14:59 +0100

    Changed how app detects if production or development.

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import config from '../config.json'
 
-const OWM_API = process.env.OWM_API || require("./secrets.json").owm_api;
+let OWM_API = (config.release) ? require("./secrets.json").owm_api : process.env.OWM_API
+    
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 
