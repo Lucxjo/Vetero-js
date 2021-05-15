75a1be636ba098f2566ea101b10245a23e9470ce
Ludoviko <louis@hollingworth.ch>
2021-05-15 15:03:39 +0100

    Added Tauri for native-wrapped apps on desktop, changed env names

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,8 +1,4 @@
 import axios from 'axios';
-import config from '../config.json'
-
-let OWM_API = (config.release) ? process.env.OWM_API : require("./secrets.json").owm_api
-    
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 
@@ -11,7 +7,7 @@ export const fetchWeather = async (query) => {
         params: {
             q: query,
             units: 'metric',
-            APPID: OWM_API
+            APPID: process.env.REACT_APP_OWM_API
         }
     });
 
